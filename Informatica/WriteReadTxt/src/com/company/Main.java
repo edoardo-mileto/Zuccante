73e2b4bf29611eac0ab760fbaf0a4d31a1157611
edoardo-mileto <3d0xit4@gmail.com>
2017-10-19 19:46:24 +0200

    aggiunte scrittura su file ed eccezioni

--- Informatica/WriteReadTxt/src/com/company/Main.java
+++ Informatica/WriteReadTxt/src/com/company/Main.java
@@ -1,27 +1,36 @@
 package com.company;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-	// write your code here
-        File inFile = new File ("input.txt");
-        Scanner inScan = new Scanner (inFile);
-        int parole=0; int linee=0;
-        while(inScan.hasNext()) {
-            inScan.next();
+        // write your code here
+        File outFile=new File ("output.txt");
+        File inFile=new File ("input.txt");
+        Scanner input;
+        PrintWriter output;
+        try {
+            output = new PrintWriter(outFile);
+            input = new Scanner(inFile);
+
+        }catch (FileNotFoundException e){
+            System.out.println("Non ho trovato il file.");
+            return;
+        }
+        int parole =0;int linee=0;
+        while(input.hasNext()) {
+            input.next();
             parole++;
         }
-        inScan=new Scanner(inFile);
-        while(inScan.hasNextLine()){
-            inScan.nextLine();
+        input=new Scanner(inFile);
+        while(input.hasNextLine()) {
+            input.nextLine();
             linee++;
         }
-        System.out.println("Parole: " + parole);
-        System.out.println("Righe: " + linee);
+        output.println("Parole: " + parole +"\nRighe: "+linee);
+        output.flush();
+        output.close();
+        input.close();
 
     }
 }
