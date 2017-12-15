ab776406830c0f87cdd990f3d7e884a7a7f2e0ce
Edoardo Mileto <3d0xit4@gmail.com>
2017-12-15 09:27:08 +0100

    Aggiunta pila in awt, da completare

---
+++ Informatica/pilaGrafica/src/AscoltatoreWin.java
@@ -0,0 +1,14 @@
+
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+class AscoltatoreWin extends WindowAdapter {
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.out.println("Chiusura");
+		System.exit(0);
+	}
+	
+}
