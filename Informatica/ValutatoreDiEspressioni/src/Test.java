09313b213ddd262274845c47550d9e4f82a63836
Edoardo Mileto <3d0xit4@gmail.com>
2017-12-15 09:28:32 +0100

    aggiunto valutatore di espressioni

---
+++ Informatica/ValutatoreDiEspressioni/src/Test.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Test {
+    public static void main (String []args) {
+        Scanner ciao = new Scanner(System.in);
+        System.out.print("wewe dimmi la stringa: ");
+        String esp= new String(ciao.nextLine());
+    }
+}
